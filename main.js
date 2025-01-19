200b42672a2d293f2fb5eadc596e256666f0c7ca
LxShadow <152157350+LxShadow@users.noreply.github.com>
2025-01-19 17:49:10 -0600

    𝐋𝐲𝐞𝐧 𝐁𝐨𝐭

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ const connectionOptions = {
     logger: Pino({ level: 'silent' }),
     printQRInTerminal: opcion === '1' || methodCodeQR,
     mobile: MethodMobile,
-    browser: opcion === '1' ? ['Shadows Bot', 'Safari', '2.0.0'] : methodCodeQR ? ['Shadows Bot', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '20.0.04'],
+    browser: opcion === '1' ? ['𝐋𝐲𝐞𝐧 𝐁𝐨𝐭', 'Safari', '2.0.0'] : methodCodeQR ? ['𝐋𝐲𝐞𝐧 𝐁𝐨𝐭', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '20.0.04'],
     auth: {
         creds: state.creds,
         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: 'fatal' }).child({ level: 'fatal' })),
@@ -531,7 +531,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn?.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑺𝒉𝒂𝒅𝒐𝒘𝑽𝒛 𝑻𝒊𝒎𝒆 𝑨𝒄𝒕𝒊𝒗𝒆𝒅: ${uptime}`;
+  const bio = `𝐌𝐲 𝐎𝐰𝐧𝐞𝐫 𝐈𝐬 𝐌𝐢𝐜𝐡𝐡 𝐕𝐞𝐧𝐭𝐚𝐬𝐬 | 𝐓𝐢𝐦𝐞 𝐀𝐜𝐭𝐢𝐯𝐞: ${uptime}`;
   await conn?.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
